6871bd0539d8e9dda4f5c3dfca617b1a24d91ef2
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-03 12:12:20 -0800

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ The flask application root """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """To handle 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
