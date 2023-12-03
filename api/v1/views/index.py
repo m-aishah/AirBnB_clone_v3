71408e7ac61c4236a6ae8ecfb8d1c905f8e473fc
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-03 11:43:58 -0800

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,15 @@ def route_status():
     return jsonify({
         "status": "OK"
         })
+
+
+@app_views.route('/stats')
+def object_no():
+    # returns the number of objects by type
+    all_objects = storage.all()
+
+    # if not all_objects:
+    #    return jsonify({"error": "No objects found in storage"}), 404
+    all_types = [obj.__class__.__name__ for obj in all_objects.values()]
+    counts = {type_: storage.count(type_) for type_ in all_types}
+    return jsonify(counts)
