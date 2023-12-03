f739fe427a9f4423c77c033ee4dfe923774210a4
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-03 04:34:10 -0800

    task 3 - fixed pycodestyle errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from flask import jsonify
-
 from api.v1.views import app_views
+
 
 @app_views.route('/status')
 def route_status():
+    # returns the route status in json
     return jsonify({
         "status": "OK"
         })
