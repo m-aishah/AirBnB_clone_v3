68dfab49e7d22a81134cdfebb435a28ea701e4dd
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-04 18:06:39 -0800

    task 6 - made some corrections to the states.py file and imported the file in the __init__.py file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,21 +6,25 @@ A new view for State objects that handles all default RESTFul API actions
 from flask import Flask, jsonify, request
 from models import storage
 from api.v1.views import app_views
+from models.state import State
+
+
+app = Flask(__name__)
 
 
 @app.route('/api/v1/states', methods=['GET'])
 def get_all_states():
     """Retrieves the list of all State objects"""
-    states = models.storage.all(State)
-    retrun jsonify([state.to_dict() for state in states.values()])
+    states = storage.all(State)
+    return jsonify([state.to_dict() for state in states.values()])
 
 
-@app.route('/api/v1/states/<int: state_id>', methods=['GET'])
+@app.route('/api/v1/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
     if not state:
-        return jsonify({"error:" "404"}), 404
+        return jsonify({"error": "404"}), 404
     return jsonify(state.to_dict())
 
 
