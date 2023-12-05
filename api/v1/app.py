09a91c7d450b3990d0f5ccf55f248f7acba1148f
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-04 23:55:24 -0800

    task 12-corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,13 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "http://0.0.0.0:*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
